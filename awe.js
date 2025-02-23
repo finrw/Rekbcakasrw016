a34a90164abfd1c44e73000090b7d969455843de
Kasrw016 <163361771+finrw@users.noreply.github.com>
2025-02-23 08:56:26 +0700

    Add files via upload

---
+++ awe.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.bottom-nav li a').click(function(){
+        var position = $(this).position();
+        var margin = 37;
+        $('.slider').css({"left":+position.left+margin ,  "transform": "translateX(-50%)"});
+        $('.bottom-nav li a').removeClass('active-icon');
+        $(this).addClass('active-icon');
+    });
+});
